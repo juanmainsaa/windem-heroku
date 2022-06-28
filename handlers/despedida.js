38404931c548f3b0a30cdbd0d17c5d44c9f5fbfb
juanmainsaa <Juanma.insaa@gmail.com>
2022-06-28 05:02:02 -0300

    Add files via upload

---
+++ handlers/despedida.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = client => {
+    console.log("Modulo de DESPEDIDA cargado!".green);
+    client.on("guildMemberRemove", (member) => {
+        const WelcomeEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Se nos fue un miembro!')
+        .setURL('https://discord.gg/sqAYjusc')
+        .setDescription(`Hasta luego <@${member.id}>! 
+        Lamentamos tu salida!`)
+        .setThumbnail(member.user.displayAvatarURL({ format: 'jpg' }))
+        .setTimestamp()
+        .setFooter({ text: member.guild.name, iconURL: member.guild.iconURL(), url: 'https://discord.gg/sqAYjusc' });
+        client.channels.cache.get("980309711364710482").send({ embeds: [WelcomeEmbed]});
+  })
+}
