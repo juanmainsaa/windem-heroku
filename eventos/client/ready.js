4f4ef5af44addb63ed04df862f0d481622f3bb7e
juanmainsaa <Juanma.insaa@gmail.com>
2022-06-28 04:27:49 -0300

    Update ready.js

--- eventos/client/ready.js
+++ eventos/client/ready.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 const config = require('../../config/config.json');
 module.exports = client => {
+    const Estado = [
+            `Carrear a tu vieja`,
+            `Carrear a Corvina`,
+            `Carrear a Kmello`,
+            `Robar comandos`,
+            `Mejorar el servidor`
+        ];
+    setInterval(() => {
+        client.user.setPresence({ activities: [{ name: Estado[Math.floor(Math.random() * Estado.length)] }], status: 'online', type: "WATCHING" })
+        }, 10000);
     //Nos conectamos a la base de datos
 
     let palo = 53;
@@ -11,9 +21,7 @@ module.exports = client => {
     }).then(() => {
         console.log(`
 ╔═════════════════════════════════════════════════════╗
-║                                                     ║
 ║       Conectado a la base de datos de MONGODB!      ║
-║                                                     ║
 ╚═════════════════════════════════════════════════════╝`.blue)
     }).catch((err) => {
         console.log(`☁ ERROR AL CONECTAR A LA BASE DE DATOS DE MONGODB`.red);
@@ -21,9 +29,7 @@ module.exports = client => {
     })
 
     console.log(`╔═════════════════════════════════════════════════════╗`.green)
-    console.log(`║ `.green + " ".repeat(-1 + palo - 1) + " ║".green)
     console.log(`║ `.green + `      Conectado como ${client.user.tag}`.green + " ".repeat(-1 + palo - 1 - `      Conectado como ${client.user.tag}`.length) + " ║".green)
-    console.log(`║ `.green + " ".repeat(-1 + palo - 1) + " ║".green)
     console.log(`╚═════════════════════════════════════════════════════╝`.green)
 }
 
